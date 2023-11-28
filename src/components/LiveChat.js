5255577bacd40ee3d49b5e9cb0c62b6957e1ef04
Keerthana <kkeerthana77@gmail.com>
2023-11-28 16:08:42 +0800

    Add files via upload

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -18,6 +18,7 @@ export default function LiveChat() {
         }, 1500)
 
         return () => clearInterval(i);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
